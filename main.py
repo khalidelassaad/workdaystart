67737b86f00a1256db6509ca48e9f070b7d1df56
Khalid Elassaad <khalidelassaad@gmail.com>
2022-06-30 16:02:03 -0700

    code: grabbing list of files and finding specific ones

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import googleapiauth
 import datetime
+import json
 
 from googleapiclient.discovery import build
 
@@ -15,6 +16,9 @@ driveService = build('drive', 'v3', credentials=creds)
 documentTitle = "{} Notes".format(datetime.date.today().strftime("%m-%d-%Y"))
 
 # 4. Does this title already exist in folder location?
+for file in driveService.files().list(corpora="user", fields="*").execute().get('files', []):
+    if file['id'] == "1Nbb96paDsE00Bq-Y87CKF_8x6cN5pM6W":
+        print(json.dumps(file))
 #   If so open it
 #   If not...
 # 5. Create document with today's title
